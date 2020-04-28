cecf85b987dd31f5bac5dcc06ede989e00afe250
Nekmo <contacto@nekmo.com>
2020-04-28 19:04:05 +0200

    Allowed ips models.

---
+++ rest_framework_security/allowed_ips/config.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+
+ALLOW_IPS_DEFAULT_UNKNOWN_IP_ACTION = ''
+
+# Override my settings usign Django Settings
+for var_name, value in dict(locals()).items():
+    if var_name.isupper():
+        locals()[var_name] = getattr(settings, var_name, value)
---
+++ rest_framework_security/allowed_ips/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from rest_framework_security.allowed_ips import config
+
+IP_ACTIONS = [
+    ('allow', _('Allow IP')),
+    ('req_2fa', _('Require multi-factor authentication')),
+    ('warn', _('Allow but send email')),
+    ('deny', _('Deny IP')),
+]
+
+
+class UserIp(models.Model):
+    ip_address = models.GenericIPAddressField()
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    action = models.CharField(max_length=12, choices=IP_ACTIONS, blank=True)
+    last_used_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class UserIpSettings(models.Model):
+    unknown_ip_action = models.CharField(max_length=12, choices=IP_ACTIONS,
+                                         default=config.ALLOW_IPS_DEFAULT_UNKNOWN_IP_ACTION)
+
+    class Meta:
+        abstract = True
