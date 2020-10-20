f74ed7136b2edf69b1fa5854a267e1a154c0353c
Nekmo <contacto@nekmo.com>
2020-10-20 12:55:53 +0200

    Only include user_ip_configs if ALLOWED_IPS_USER_IP_CONFIG_MODEL is defined.

--- rest_framework_security/allowed_ips/urls.py
+++ rest_framework_security/allowed_ips/urls.py
@@ -2,11 +2,14 @@ from django.conf.urls import url
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from rest_framework_security.allowed_ips import config
 from rest_framework_security.allowed_ips.views import UserIpConfigViewSet, UserIpViewSet
 
 router = DefaultRouter()
 router.register('user_ips', UserIpViewSet)
-router.register('user_ip_configs', UserIpConfigViewSet, basename='user_ip_config')
+
+if config.ALLOWED_IPS_USER_IP_CONFIG_MODEL:
+    router.register('user_ip_configs', UserIpConfigViewSet, basename='user_ip_config')
 
 
 urlpatterns = [
