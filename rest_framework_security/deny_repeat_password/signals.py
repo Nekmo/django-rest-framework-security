84fbca40f061db8a8894655cb41e418d892407c9
Nekmo <contacto@nekmo.com>
2020-10-28 19:06:32 +0100

    Issue #1: Detect a password change by hasher change

--- rest_framework_security/deny_repeat_password/signals.py
+++ rest_framework_security/deny_repeat_password/signals.py
@@ -13,6 +13,8 @@ password_changed = Signal(providing_args=['user'])
 
 @receiver(post_save, sender=get_user_model())
 def set_user_password_receiver(sender, instance, **kwargs):
+    if instance._password is None:
+        return
     user_password, created = UserPassword.objects.get_or_create(user=instance, password=instance.password)
     UserPassword.objects.remove_old_passwords(instance)
     if created and config.DENY_REPEAT_PASSWORD_CLOSE_SESSIONS \
