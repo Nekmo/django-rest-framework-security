4b23327a620e7d920753586f51de7f54cfa54607
Nekmo <contacto@nekmo.com>
2021-01-03 00:39:57 +0100

    Issue #2: Write tests (LoginProtectionViewSetTestCase).

--- rest_framework_security/brute_force_protection/tests.py
+++ rest_framework_security/brute_force_protection/tests.py
@@ -5,6 +5,9 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
+from rest_framework_security.brute_force_protection import config
+from rest_framework_security.brute_force_protection.protection import BruteForceProtection
+
 
 class CaptchaViewSetTestCase(APITestCase):
     def setUp(self) -> None:
@@ -30,3 +33,31 @@ class CaptchaViewSetTestCase(APITestCase):
     def test_valid(self):
         response = self.client.post(self.url, {'recaptcha_response': 'foo'}, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class LoginProtectionViewSetTestCase(APITestCase):
+    def setUp(self) -> None:
+        self.url = reverse('brute_force_protection-status')
+
+    def test_allowed(self):
+        response = self.client.get(self.url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json().get('status'), 'allowed')
+
+    def test_banned(self):
+        protection = BruteForceProtection('127.0.0.1')
+        protection.delete_ip()
+        for i in range(config.BRUTE_FORCE_PROTECTION_BAN_LIMIT):
+            protection.increase_attempts()
+        response = self.client.get(self.url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json().get('status'), 'banned')
+
+    def test_captcha_required(self):
+        protection = BruteForceProtection('127.0.0.1')
+        protection.delete_ip()
+        for i in range(config.BRUTE_FORCE_PROTECTION_SOFT_LIMIT):
+            protection.increase_attempts()
+        response = self.client.get(self.url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json().get('status'), 'captcha_required')
